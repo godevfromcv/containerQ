133f0e3ccf02c29460803570985940ca866de0a6
godevfromcv <pythondevluc7@gmail.com>
2024-04-05 10:45:37 +0300

    fix

--- main.go
+++ main.go
@@ -2,32 +2,23 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/sys/unix"
 	"os"
 	"os/exec"
-	"syscall"
 )
 
-func run() {
-	cmd := exec.Command("sudo /bin/sh")
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+func main() {
+	// Ваш код тут
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &unix.SysProcAttr{
+		Cloneflags: unix.CLONE_NEWNS | unix.CLONE_NEWUTS | unix.CLONE_NEWPID,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
-		os.Exit(1)
-	}
-}
-
-func main() {
-	switch os.Args[1] {
-	case "run":
-		run()
-	default:
-		fmt.Println("unknown command")
+		fmt.Fprintf(os.Stderr, "Failed to run the command: %v\n", err)
 		os.Exit(1)
 	}
 }
